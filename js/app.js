25c5da6d94d402a3738d20cc1d854f52c42bcecb
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-08-27 21:10:24 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const apiUrl = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
 
-//This Map will act like our DataBase which also help speed up in deleting , searching and editing
+
+//This Array will act like our DataBase
 let userData = [];
 
 //Function will help us to make the get request on url and abl to fetch the data
@@ -30,17 +31,30 @@ const getData = async() => {
 let pageSize = 10;
 let currentPage = 1;
 
-async function viewTable(currentPage)
+async function viewTable()
 {
 	//get the data
 	await getData();
 	let rows = "";
 
+	if(currentPage==1) 
+	{
+		document.querySelector("#toFirstPage").setAttribute("disabled",true);
+		document.querySelector("#prevFunc").setAttribute("disabled",true);
+	}
+	console.log("Inside",currentPage)
+
+	if(currentPage>1)
+	{
+		console.log("Inside >1",currentPage)
+		document.querySelector("#toFirstPage").setAttribute("disabled",false);
+		document.querySelector("#prevFunc").setAttribute("disabled",false);
+	}
+
 	//Now we will traverse to the array but we will follow the limitation also
-
 	let begin=(currentPage-1)*pageSize;
-	let end=currentPage*pageSize;
-
+	let end=currentPage*pageSize>userData.length ? userData.length : currentPage*pageSize;
+	
 	for(let user=begin;user<end;user++)
 	{
 		rows+="<tr>"
@@ -50,14 +64,14 @@ async function viewTable(currentPage)
 		rows+=`<td> ${(userData[user].role)} </td>`
 		rows+=`<td>
 		<button class="edit" title="Edit" data-toggle="tooltip"> ✏ </button>
-		<button class="delete" id="delete" title="Delete" data-toggle="tooltip"> 🗑️ </button>
-	</td>`
+		<button class="delete" id="delete" title="Delete" data-toggle="tooltip"> 🗑️ </button></td>`
 		"</tr>"
 	}
+
 	document.getElementById("data").innerHTML=rows;
     
 	//Now here I have written logic for showing boxes of pageJumper
-	let pagesBtnNumSize=0; //Limitation for showing number of boxe (like once only 4 boxes allowed)
+	let pagesBtnNumSize=0; //Limitation for showing number of boxs (like once only 4 boxes allowed)
 
 	//Forexample if we have data arround 46 then 5 pages required but at one page I am allowing only four
 	//Boxes
@@ -86,6 +100,7 @@ async function viewTable(currentPage)
 		
 		pagesBtnNumSize++;
 		}
+
 		else
 		{
 			pagesBtnNumSize=0;
@@ -93,39 +108,60 @@ async function viewTable(currentPage)
 	}
 
 	document.getElementById("pagesJumper").innerHTML=pagesBtnNumCol;
-	console.log(userData.length)
 }
+viewTable()
 
-viewTable(1)
-
-const prevPage = () => {
+//Through this function we can move prevpage
+const prevPage=()=>{
 	if(currentPage>1)
 		currentPage--;
 	
-	viewTable(currentPage);
+	console.log("Prec => ",currentPage);
+	viewTable();
 }
 
 const nextPage=()=>{
 	if(currentPage*pageSize < userData.length) currentPage++;
-
-	viewTable(currentPage);
+	console.log("Next => ",currentPage)
+	viewTable();
 }
 
 const deleteUserById=(id)=>{
    userData.forEach(user=>{
         if(user.id==id) delete user;
-
    })
+   viewTable();
+}
 
-   viewTable(currentPage);
+const editUserById=(id,name,email,role)=>{
+	userData.forEach(user=>{
+		if(user.id==id)
+		{
+			user.name=name;
+			user.email=email;
+			user.role=role;
+		}
+	})
+	viewTable(currentPage);
+}
+
+const toFirstPage=()=>{
+	console.log("active");
+	viewTable();
+}
+//For Moving to previous Page
+document.querySelector("#prevFunc").addEventListener('click', prevPage);
+//For moving to next Page
+document.querySelector("#nextFunc").addEventListener('click', nextPage);
+
+//Moving to FirstPage
+document.querySelector("#toFirstPage").addEventListener('click',toFirstPage);
+
+const toLastPage=()=>{
+	
 }
 
 
-
-//For Moving to previous Page
-document.querySelector("#prevFunc").addEventListener('click', prevPage,false);
-//For moving to next Page
-document.querySelector("#nextFunc").addEventListener('click', nextPage,false);
-
 //For Deleteing the Page
 // document.querySelector("#delete").addEventListener('delete', deleteUserById,false);
+
