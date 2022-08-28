00ae7e61848db90d89072cd1391d256d11a36ae8
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-08-28 16:35:10 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const apiUrl = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-
-
+// Define Head Selector For Selecting items
+const headSelector=document.querySelector(".headSelector");
 //This Array will act like our DataBase
 let userData = [];
+
+//This Section is for the filltered Item
+let filteredData=[];
+let searchString="";
+let byFilter=false;
 
 //Function will help us to make the get request on url and abl to fetch the data
 const getData = async() => {
@@ -27,6 +32,7 @@ const getData = async() => {
 
 //For Changing the Properties of Button according to the Page Number
 const putLimitOnBtn=()=>{
+
 	//For Changing the Properties of Buttons
 	let toFirstPageBtn=document.querySelector("#toFirstPage")
 	let toLastPageBtn=document.querySelector("#toLastPage")
@@ -50,8 +56,7 @@ const putLimitOnBtn=()=>{
 		backBtn.disabled=false;
 		backBtn.classList.remove("btn-secondary");
 	}
-
-	if(currentPage==totalPage())
+    if(currentPage==totalPage(byFilter))
 	{
 		toLastPageBtn.disabled=true;
 		toLastPageBtn.classList.add("btn-secondary");
@@ -59,7 +64,7 @@ const putLimitOnBtn=()=>{
 		nextBtn.classList.add("btn-secondary");
 	}
 
-	if(currentPage<totalPage())
+	if(currentPage<totalPage(byFilter))
 	{
 		toLastPageBtn.disabled=false;
 		toLastPageBtn.classList.remove("btn-secondary");
@@ -75,12 +80,15 @@ const pageJumper=()=>{
 
 	//Forexample if we have data arround 46 then 5 pages required but at one page I am allowing only four
 	//Boxes
+	let dataBase=[];
+	if(byFilter) dataBase=filteredData;
+	else dataBase=userData;
 
 	let pagesBtnNumCol="";
 	let startPoint=currentPage>=5 ? currentPage : 1;
 	
-	let btnSize=Math.floor(userData.length/pageSize);
-	let endPoint = userData.length > btnSize*pageSize ? btnSize+1 : btnSize;
+	let btnSize=Math.floor(dataBase.length/pageSize);
+	let endPoint = dataBase.length > btnSize*pageSize ? btnSize+1 : btnSize;
 
 	if(currentPage>=5) startPoint=currentPage;
 
@@ -90,12 +98,12 @@ const pageJumper=()=>{
 		{
 		if(currentPage==pages)
 			pagesBtnNumCol+=`<li class="page-item active">
-			<button class="page-link"  onclick=jumpTo(${(pages)})>${pages}</button>
+			<button class="page-link"  onclick=jumpTo(${(pages)})> ${pages} </button>
 		</li>`
 		
 		else 
 			pagesBtnNumCol+=`<li class="page-item">
-			<button class="page-link"  onclick=jumpTo(${(pages)})>${pages}</button>
+			<button class="page-link"  onclick=jumpTo(${(pages)})> ${pages} </button>
 		</li>`
 		
 		pagesBtnNumSize++;
@@ -116,10 +124,10 @@ const pageJumper=()=>{
 let pageSize = 10;
 let currentPage = 1;
 
-let isFetched = false;
 
+let isFetched = false;
 async function viewTable()
-{
+{	
 	//get the data
 	if(!isFetched)
 	{
@@ -127,38 +135,90 @@ async function viewTable()
 		await getData()
 	}
 
+	let dataBase=[];
+
+	if(byFilter)
+	{
+		dataBase=filteredData;
+		
+	}
+	else dataBase=userData;
+
 	//Set the Buttons Limitations
 	putLimitOnBtn();
 	
     let rows = "";
-	if(userData.length)
+	if(dataBase.length)
 	{
 	//Now we will traverse to the array but we will follow the limitation also
 	let begin=(currentPage-1)*pageSize;
-	let end=currentPage*pageSize>userData.length ? userData.length : currentPage*pageSize;
+	let end=currentPage*pageSize>dataBase.length ? dataBase.length : currentPage*pageSize;
 	
 	for(let user=begin;user<end;user++)
 	{
-		rows+="<tr>"
-		rows+=`<td><input type="checkbox" class="form-check-input" id="exampleCheck1"></td>`
-		rows+=`<td> ${(userData[user].name)} </td>`
-		rows+=`<td> ${(userData[user].email)} </td>`
-		rows+=`<td> ${(userData[user].role)} </td>`
+		rows+="<tr class='trs'>"
+		rows+=`<td><input type="checkbox" class="form-check-input inputs" onchange='singleInputSelected(event,${dataBase[user].id})' id="exampleCheck1" key=${dataBase[user].id}></td>`
+		rows+=`<td > ${(dataBase[user].name)} </td>`
+		rows+=`<td> ${(dataBase[user].email)} </td>`
+		rows+=`<td> ${(dataBase[user].role)} </td>`
 		rows+=`<td>
-		<button class="edit" title="Edit" data-toggle="tooltip"> ✏ </button>
-		<button class="delete" id="delete" title="Delete" data-toggle="tooltip" onclick=deleteUserById(${(userData[user].id)})> 🗑️ </button></td>`
+		<button class="edit" onclick=doEditing(event,${dataBase[user].id})  title="Edit" data-toggle="tooltip"> ✏ Edit </button>
+		<button class="delete" id="delete" title="Delete" data-toggle="tooltip" onclick=deleteUserById(${(dataBase[user].id)})> 🗑️ Delete </button></td>`
 		"</tr>"
 	}
     }
 
 	else rows="<h3 class='d-flex justify-content-center'> No Record Found 😩</h3>";
 	document.getElementById("data").innerHTML=rows;
-    
 
-pageJumper()
-	
+	pageJumper()
+	if(headSelector.checked) headSelector.checked=false;
 }
 viewTable()
+
+ 
+
+ // Do Editing Function Define
+ let isClicked=1;
+ const doEditing=(event,id)=>{
+
+	isClicked+=1;
+	if(isClicked%2==0){
+		// Set Editable true
+		event.target.innerHTML="💾 Save"
+		const childs=event.path[2].children;
+		for(let i=1;i<childs.length-1;i++){
+			childs[i].setAttribute("contenteditable",true);
+			childs[i].style.backgroundColor="lightgray";
+		}
+	}
+	else{
+		let name="",email="",role="";
+		// Set Editable False
+		event.target.innerHTML="✏ Edit"
+		event.target.style.opacity="1";
+		const childs=event.path[2].children;
+		for(let i=1;i<childs.length-1;i++){
+			childs[i].removeAttribute("contenteditable");
+			childs[i].style.backgroundColor="white"
+			switch(i){
+				case 1:
+					name=childs[i].innerText;
+					break;
+				case 2:
+					email=childs[i].innerText;
+					break;
+				case 3:
+					role=childs[i].innerText;
+					break;
+				default:
+					break;
+			}
+		}
+		editUserById(id,name,email,role);
+	}
+
+ }
 
 //Through this function we can move prevpage
 const prevPage=()=>{
@@ -184,9 +244,8 @@ const deleteUserById=(id)=>{
 	{
 		if(userData[user].id==id) break;
 	}
-   console.log(user);
    userData.splice(user,1);
-   console.log(userData)
+   if(byFilter) resetFilteredArr()
    viewTable();
 }
 
@@ -199,6 +258,8 @@ const editUserById=(id,name,email,role)=>{
 			user.role=role;
 		}
 	})
+	//Reseting the Filter Array if this call come after filtering
+	if(byFilter) resetFilteredArr()
 	viewTable();
 }
 
@@ -217,8 +278,12 @@ const jumpTo=(page)=>{
 }
 
 const totalPage=()=>{
-	let totalPage = Math.floor(userData.length/pageSize);
-	return userData.length> totalPage*pageSize? totalPage+1 : totalPage;
+	let dataBase=[]
+	if(byFilter) dataBase=filteredData;
+	else dataBase=userData;
+
+	let totalPage = Math.floor(dataBase.length/pageSize);
+	return dataBase.length> totalPage*pageSize? totalPage+1 : totalPage;
 }
 
 const toLastPage=()=>{
@@ -227,3 +292,91 @@ const toLastPage=()=>{
 }
 //Moving to Last Page
 document.querySelector("#toLastPage").addEventListener('click', toLastPage);
+
+let toDeleteId=[]
+let clickOnMainCheckBox=1;
+let isDelete=0;
+//For Selecting All
+const selectAll =()=>{
+    clickOnMainCheckBox++;
+	if(clickOnMainCheckBox%2==0)
+	{
+		isDelete=1;
+		const allInputs=document.querySelectorAll(".inputs");
+		allInputs.forEach((input)=>{
+			toDeleteId.push(+input.getAttribute("key"));
+			input.checked=true;
+		})
+	}
+	else{
+		clickOnMainCheckBox=1;
+		isDelete=0;
+		const allInputs=document.querySelectorAll(".inputs");
+		allInputs.forEach((input)=>{
+			toDeleteId.splice(+input.getAttribute("key"),1);
+			input.checked=false;
+		})
+	}
+}
+
+const deleteSlectedUser=()=>{
+	if(currentPage==totalPage()) currentPage--;
+	
+	// if(isDelete)
+	// {
+	clickOnMainCheckBox=1;
+	toDeleteId.forEach(id=>{
+		userData.forEach(user=>{
+			if(user.id==id)
+			{   
+				userData.splice(userData.indexOf(user),1);
+			}
+		})
+	})
+	
+	toDeleteId.splice(0,toDeleteId.length)
+	resetFilteredArr();
+	viewTable();
+//    }
+}
+
+const singleInputSelected=(event,id)=>{
+	if(event.target.checked){
+		// Add
+		toDeleteId.push(id)
+	}
+	else{
+		// 
+		let targetIndex=toDeleteId.indexOf(id)
+		toDeleteId.splice(targetIndex,1)
+		}		
+		//console.log(toDeleteId)
+}
+
+const searchItem=(e)=>{
+	currentPage=1;
+	byFilter=true
+	filteredData.splice(0,filteredData.length)
+	searchString=e.target.value.toLowerCase();
+	let resultant=userData.filter(data=>{
+
+		let fullName=data.name.toLowerCase();
+		let nameArr=fullName.split(" ");
+		return data.role.toLowerCase().search(searchString)!=-1||data.email.toLowerCase().search(searchString)!=-1||fullName.search(searchString)!=-1||nameArr[0]==searchString||nameArr[1]==searchString||fullName==searchString||data.role.toLowerCase()==searchString||data.email.toLowerCase()==searchString;
+	});
+	filteredData.push(...resultant);
+	viewTable();
+}
+
+const resetFilteredArr=()=>
+{
+	currentPage=1;
+	filteredData.splice(0,filteredData.length)
+	let resultant=userData.filter(data=>{
+		let fullName=data.name.toLowerCase();
+		let nameArr=fullName.split(" ");
+		return data.role.toLowerCase().search(searchString)!=-1||data.email.toLowerCase().search(searchString)!=-1||fullName.search(searchString)!=-1||nameArr[0]==searchString||nameArr[1]==searchString||fullName==searchString||data.role.toLowerCase()==searchString||data.email.toLowerCase()==searchString;
+	});
+	filteredData.push(...resultant);
+
+}
